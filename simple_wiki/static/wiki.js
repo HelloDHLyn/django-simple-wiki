fe718f26b8c9457721ceb14f041cde1df52f3295
Do Hoerin <hellodhlyn@gmail.com>
2016-08-16 18:52:31 +0900

    New feature: table of contents of article

--- simple_wiki/static/wiki.js
+++ simple_wiki/static/wiki.js
@@ -1,6 +1,22 @@
 function onClickMenu() {
-	$('.ui.sidebar')
+	$("#main-sidebar")
 		.sidebar('setting', 'transition', 'overlay')
-		.sidebar('toggle')
-	;
+		.sidebar('toggle');
 }
+
+$(document).ready(function() {
+	$("#table-of-contents").toc({
+		'selectors': 'h1,h2,h3',
+		'prefix': 'toc'
+	});
+
+	$("#toggle-toc").on('click', function() {
+		$("#toc-sidebar")
+			.sidebar('setting', 'transition', 'overlay')
+			.sidebar('toggle');
+	});
+
+	$("#table-of-contents a").on('click', function() {
+		$("#toc-sidebar").sidebar('toggle');
+	});
+});
